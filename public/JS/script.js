cd1863f4649dc30ef6a063fea879fcd5c82d1237
luana22maria <luanamc784@gmail.com>
2025-11-11 19:47:42 -0300

    atualização da hora do ropapé

--- public/JS/script.js
+++ public/JS/script.js
@@ -1,4 +1,6 @@
  // Script jQuery 
+
+ // aba de comentários
 $(document).ready(function() {
             $('#esconder').click(function() {
                 $('#minhadiv').hide();      
@@ -12,33 +14,15 @@ $(document).ready(function() {
                 $('#mostrar').hide();
               
             });    
+  });
 
 
-           
-});
+let today = new Date();
+let dayName = new Array ("Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado");
+let monName = new Array ("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Agosto","Setembro", "Outubro", "Novembro", "Dezembro");
 
-
-var today = new Date();
- var dayName = new Array ("Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado");
-var monName = new Array ("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Agosto","Setembro", "Outubro", "Novembro", "Dezembro");
-
-var formattedDate =   dayName[today.getDay()] + ", " + today.getDate() + " de " + monName[today.getMonth()] + " de " + today.getFullYear(); 
+let formattedDate =   dayName[today.getDay()] + ", " + today.getDate() + " de " + monName[today.getMonth()] + " de " + today.getFullYear(); 
 
 document.getElementById("mostra_data").innerText = formattedDate;
 
-// atualiza a hora.
-
-$(document).ready(function(){
-  function Atualiza_hora() {
-    var agora = new Date();
-    var horaFormatada = agora.toLocaleString();
-    $("#hora").text(horaFormatada);
-
-    console.log( agora );
-}
-
-  
-  Atualiza_hora(); // Chama a função para exibir a hora imediatamente
-  setInterval(Atualiza_hora, 1000); // Atualiza a hora a cada segundo
-});
 
